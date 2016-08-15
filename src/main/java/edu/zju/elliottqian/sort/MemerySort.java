a6c50036e342fa501964468024c059f238a95164
elliottqian <752004979@qq.com>
2016-08-15 11:42:21 +0800

    myleetcode

--- src/main/java/edu/zju/elliottqian/sort/MemerySort.java
+++ src/main/java/edu/zju/elliottqian/sort/MemerySort.java
@@ -8,10 +8,10 @@ public class MemerySort {
 
     public static void main(String[] args) {
         MemerySort m = new MemerySort();
-        int[] a = {1, 3, 5, 7, 10};
-        int[] b = {2, 4, 5, 8, 11};
+        int[] a = {2, 1, 5, 3, 8, 2, 4, 5, 8, 11};
+        int[] b = {};
 
-        int[] x = m.MemeryArray(a, b);
+        int[] x = m.mergeSort(a, 0, a.length);
 
         for (int i = 0; i < x.length; ++ i) {
             System.out.println(x[i]);
@@ -19,7 +19,56 @@ public class MemerySort {
     }
 
 
-    public int[] MemeryArray(int[] a, int[] b) {
+    public int[] mergeSort(int[] array, int first, int last) {
+        int middle = (last + first) / 2;
+        if (last - first > 1) {
+            System.out.println("--------------------------");
+            System.out.println(first);
+            System.out.println(last);
+            System.out.println("--------------------------");
+            mergeSort(array, first, middle);
+            mergeSort(array, middle, last);
+            mergeArray(array, first, middle, last);
+        }
+        return array;
+    }
+
+
+    public void mergeArray(int[] array, int first, int middle, int last) {
+        //   0        1      2       3        4         5
+        //   first                 middle                   last
+        int i = first;
+        int j = middle;
+        int k = 0;
+
+        int[] tmp = new int[last - first];
+
+        while (i < middle && j < last) {
+            if (array[i] < array[j]) {
+                tmp[k ++] = array[i ++];
+            } else {
+                tmp[k ++] = array[j ++];
+            }
+        }
+
+        while (i < middle) {
+            tmp[k ++] = array[i ++];
+        }
+
+        while (j < last) {
+            tmp[k ++] = array[j ++];
+        }
+
+        k = first;
+        for (int index = 0; index < tmp.length; ++index) {
+            array[k] = tmp[index];
+            k ++;
+        }
+
+    }
+
+
+    public int[] MegeArray(int[] a, int[] b) {
 
         int a_l = a.length;
         int b_l = b.length;
